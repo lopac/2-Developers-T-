06e129f830faaffcd8556a589325eaf1920ebc4a
MilanKovacic <milankovacicmk@outlook.com>
2016-05-31 10:40:11 +0200

    Reorganize ViewModels

--- IndirectlyApp/Controllers/ManageController.cs
+++ IndirectlyApp/Controllers/ManageController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using IndirectlyApp.Models;
+using IndirectlyApp.ViewModels;
 
 namespace IndirectlyApp.Controllers
 {
--- IndirectlyApp/ViewModels/ManageViewModels.cs
+++ IndirectlyApp/ViewModels/ManageViewModels.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNet.Identity;
 using Microsoft.Owin.Security;
 
-namespace IndirectlyApp.Models
+namespace IndirectlyApp.ViewModels
 {
     public class IndexViewModel
     {
