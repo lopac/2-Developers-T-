f44e9e3abc17d9f144e351dc588a0113d71fac6e
MilanKovacic <milankovacicmk@outlook.com>
2016-05-31 16:32:49 +0200

    Lopac commit

---
+++ IndirectlyApp/Scripts/draggable.js
@@ -0,0 +1,11 @@
+﻿$(function() {
+    $( "#item1" ).draggable({ containment: "#containment-wrapper", scroll: false });
+
+    $("#click").click(function(){
+      var p = $("#item1");
+      
+        //  alert(p.offsetLeft, p.offsetTop);
+        alert(JSON.stringify(p.position()));
+    });
+
+  });
