bc9131bab67d165b0a4d30a12fbde72132b94655
MilanKovacic <milankovacicmk@outlook.com>
2016-05-31 10:58:25 +0200

    Add Comment & Mosaic model

---
+++ IndirectlyApp/Models/Comment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IndirectlyApp.Models
+{
+    public class Comment
+    {
+        public string Body { get; set; }
+        public virtual ApplicationUser User { get; set; }
+        public virtual Mosaic Mosaic { get; set; }
+
+    }
+}
