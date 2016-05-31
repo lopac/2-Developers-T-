7e35ffecb118cefb286d5c9ca3e88070b913de04
MilanKovacic <milankovacicmk@outlook.com>
2016-05-31 17:12:12 +0200

    Fix draggable javascript

--- IndirectlyApp/Scripts/draggable.js
+++ IndirectlyApp/Scripts/draggable.js
@@ -7,5 +7,12 @@
         //  alert(p.offsetLeft, p.offsetTop);
         alert(JSON.stringify(p.position()));
     });
+    $(window).resize(function () {
+        
+        var containerWidth = $("#containment-wrapper").width();
 
+        //alert(container);
+        //var slika1 = ;
+        $("#item1").css("width", $("#item1").width() * (containerWidth / 500));
+    });
   });
